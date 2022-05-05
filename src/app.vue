b048212491deb29c5b1b080c794dc048da3fc397
krox2 <developer-krox@yandex.ru>
2022-05-05 23:50:58 +0300

    commit-050522-2350 - Создан проект с помощью Webpack

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Привет, мир',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+    width: 80vw;
+    margin: 0 auto;
+
+}
+h1 {
+  font-size: 40px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+});
+
+// import { createApp } from 'vue'
+// import App from './app'
+//
+// createApp(App).mount('#app')
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// const app = createApp(App);
+// app.mount('#app')
